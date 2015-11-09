78c108e91697530a2b8b07f3dd5307afd61b91d1
victorBaro <fs.baro@gmail.com>
2015-11-09 12:42:45 +0100

    Rename

--- Client/templates/RandomTVTemplate.xml.js
+++ Client/templates/RandomTVTemplate.xml.js
@@ -2,7 +2,7 @@ var Template = function() { return `<?xml version="1.0" encoding="UTF-8" ?>
   <document>
     <catalogTemplate>
       <banner>
-        <title>RandomInstagram</title>
+        <title>RandomTV</title>
       </banner>
     </catalogTemplate>
   </document>`
