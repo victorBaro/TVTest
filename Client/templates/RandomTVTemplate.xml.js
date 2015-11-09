1c0f75a0dd27390aaa05dc40d10adf39d2e1ab61
victorBaro <fs.baro@gmail.com>
2015-11-09 14:34:06 +0100

    client working

--- Client/templates/RandomTVTemplate.xml.js
+++ Client/templates/RandomTVTemplate.xml.js
@@ -17,44 +17,20 @@ var Template = function() { return `<?xml version="1.0" encoding="UTF-8" ?>
 	        <section> 
                   //2
 		  <lockup videoURL="http://www.rwdevcon.com/videos/Ray-Wenderlich-Teamwork.mp4">
-		    <img src="${this.BASEURL}images/ray.png" width="500" height="308" />
+		    <img src="${this.BASEURL}images/jc1.png" width="500" height="308" />
 		  </lockup>
 		  <lockup videoURL="http://www.rwdevcon.com/videos/Ryan-Nystrom-Contributing.mp4">
-		    <img src="${this.BASEURL}images/ryan.png" width="500" height="308" />
+		    <img src="${this.BASEURL}images/jc2.png" width="500" height="308" />
 		  </lockup>									
 	          <lockup videoURL="http://www.rwdevcon.com/videos/Matthijs-Hollemans-Math-Isnt-Scary.mp4">
-		    <img src="${this.BASEURL}images/matthijs.png" width="500" height="308" />
+		    <img src="${this.BASEURL}images/jc3.png" width="500" height="308" />
 		  </lockup>									
 		  <lockup videoURL="http://www.rwdevcon.com/videos/Vicki-Wenderlich-Identity.mp4">
-		    <img src="${this.BASEURL}images/vicki.png" width="500" height="308" />
+		    <img src="${this.BASEURL}images/jc4.png" width="500" height="308" />
 		  </lockup>									
 	          <lockup videoURL="http://www.rwdevcon.com/videos/Alexis-Gallagher-Identity.mp4">
-		    <img src="${this.BASEURL}images/alexis.png" width="500" height="308" />
+		    <img src="${this.BASEURL}images/jc5.png" width="500" height="308" />
 	          </lockup>									
-		  <lockup videoURL="http://www.rwdevcon.com/videos/Marin-Todorov-RW-Folklore.mp4">
-		    <img src="${this.BASEURL}images/marin.png" width="500" height="308" />
-		  </lockup>									
-		  <lockup videoURL="http://www.rwdevcon.com/videos/Chris-Wagner-Craftsmanship.mp4">
-		    <img src="${this.BASEURL}images/chris.png" width="500" height="308" />
-	          </lockup>									
-		  <lockup videoURL="http://www.rwdevcon.com/videos/Cesare-Rocchi-Cognition.mp4">
-		    <img src="${this.BASEURL}images/cesare.png" width="500" height="308" />
-		  </lockup>									
-		  <lockup videoURL="http://www.rwdevcon.com/videos/Ellen-Shapiro-Starting-Over.mp4">
-		    <img src="${this.BASEURL}images/ellen.png" width="500" height="308" />
-		  </lockup>									
-		  <lockup videoURL="http://www.rwdevcon.com/videos/Jake-Gundersen-Opportunity.mp4">
-		    <img src="${this.BASEURL}images/jake.png" width="500" height="308" />
-		  </lockup>									
-		  <lockup videoURL="http://www.rwdevcon.com/videos/Kim-Pedersen-Finishing.mp4">
-		    <img src="${this.BASEURL}images/kim.png" width="500" height="308" />
-		  </lockup>									
-	          <lockup videoURL="http://www.rwdevcon.com/videos/Tammy-Coron-Possible.mp4">
-		    <img src="${this.BASEURL}images/tammy.png" width="500" height="308" />
-		  </lockup>									
-		  <lockup videoURL="http://www.rwdevcon.com/videos/Saul-Mora-NSBrief.mp4">
-		    <img src="${this.BASEURL}images/saul.png" width="500" height="308" />
-		  </lockup>		
 		</section>
 	      </grid>
 	    </relatedContent>
